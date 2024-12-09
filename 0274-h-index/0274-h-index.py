8d8e829867bdb479f88208bce1d91e1631126572
Sowmyadevalla2005 <128302736+Sowmyadevalla2005@users.noreply.github.com>
2024-12-09 22:12:26 +0530

    Time: 0 ms (100%), Space: 17.5 MB (11.19%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,9 +1,17 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
         n=len(citations)
-        for i in range(n):
-            if citations[i]<=i:
-                return i
-        return n
+        freq=[0]*(n+1)
+        for c in citations:
+            if c>=n:
+                freq[n]+=1
+            else:
+                freq[c]+=1
+        
+        idx=n
+        count=freq[n]
+        while count<idx:
+            idx-=1
+            count=count+freq[idx]
+        return idx
         
