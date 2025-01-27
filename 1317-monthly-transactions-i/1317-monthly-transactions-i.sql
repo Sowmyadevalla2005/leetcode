d015ddd7d9fea676cef1cd07efde6f162d7652da
Sowmyadevalla2005 <128302736+Sowmyadevalla2005@users.noreply.github.com>
2025-01-27 22:20:25 +0530

    Time: 1141 ms (33.2%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-SELECT 
-    DATE_FORMAT(trans_date , '%Y-%m') AS month,
-    country,
-    COUNT(id) AS trans_count,
-    SUM(state = 'approved') AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM(IF(state = 'approved' , amount , 0)) AS approved_total_amount
-FROM Transactions
-GROUP BY month , country
+
+select DATE_FORMAT(trans_date, '%Y-%m') as month,
+country,
+count(id) as trans_count,
+sum(state='approved') as approved_count,
+sum(amount) as trans_total_amount,
+sum(IF(state='approved',amount,0)) as approved_total_amount
+from Transactions
+group by month ,country
