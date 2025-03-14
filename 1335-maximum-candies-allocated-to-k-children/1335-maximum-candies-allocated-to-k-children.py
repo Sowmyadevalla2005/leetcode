37ddd88a3535ebba4e49fec6411ffeeb75d6e5d7
Sowmya <128302736+Sowmyadevalla2005@users.noreply.github.com>
2025-03-14 15:20:18 +0530

    Time: 155 ms (100%), Space: 20.3 MB (22.22%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -1,21 +1,30 @@
-class Solution:
-    def maximumCandies(self, candies: List[int], k: int) -> int:
+class Solution(object):
+    def maximumCandies(self, candies, k):
+        """
+        :type candies: List[int]
+        :type k: int
+        :rtype: int
+        """
 
         t=sum(candies)
         if t<k:
             return 0
-
-        l=1
-        r=t//k
+        
+        l,r=1,t//k
 
         while(l<r):
-            mid = (l+r+1)//2
+            mid=(l+r+1)//2
 
-            count=sum(candy//mid for candy in candies)
+            count = sum(candy//mid for candy in candies)
 
             if count>=k:
                 l = mid
-            else:
-                r=mid-1
 
-        return l        
+            else:
+                r = mid-1
+        
+        return l
+
+
+
+        
