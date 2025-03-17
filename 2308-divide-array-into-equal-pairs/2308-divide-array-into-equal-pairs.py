1a5599027e6b741f672a3b361f42e2a8683ed9f2
Sowmya <128302736+Sowmyadevalla2005@users.noreply.github.com>
2025-03-17 21:50:51 +0530

    Time: 7 ms (54.26%), Space: 12.5 MB (65.96%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+            
+        nums.sort()
+
+        return all(nums[i]==nums[i+1] for i in range(0,len(nums),2))
+
+        
