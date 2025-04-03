eed3c438814a411eefe644b0fb92dc43f71eac7a
Sowmya <128302736+Sowmyadevalla2005@users.noreply.github.com>
2025-04-03 21:55:16 +0530

    Time: 0 ms (100%), Space: 12.8 MB (25.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,14 +5,31 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-
         m=len(matrix)
         n=len(matrix[0])
 
-        for i in range(m):
-            for j in range(n):
-                if target == matrix[i][j]:
-                    return True
+        s=m*n
+        l=0
+        r=s-1
+
+        while l<=r:
+
+            mid=(l+r)//2
+            m_i=mid//n
+            m_j=mid%n
+
+            if target == matrix[m_i][m_j]:
+                return True
+            
+            elif target < matrix[m_i][m_j]:
+                r = mid-1
+
+            else:
+                l = mid+1
 
         return False
+
+
+
+
         
