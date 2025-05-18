7fac83b4eb7ab3f6622fe8dc1e3b98cbb328abae
Sowmya <128302736+Sowmyadevalla2005@users.noreply.github.com>
2025-05-18 09:14:20 +0530

    Time: 4 ms (5.04%), Space: 12.7 MB (41.74%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,9 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        for i in range(len(nums)):
-            if target == nums[i]:
-                return i
+        l=0
+        r=len(nums)-1
+        if target not in nums:
+            return -1
+
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+
+            if nums[l]<=nums[mid]:
+                if nums[l]<=target<=nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            else:
+                if nums[mid]<=target<=nums[r]:
+                    l=mid+1
+                else:
+                    r=mid-1
         return -1
-        
